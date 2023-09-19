2ff16aac51b0e92c7728bc9f20bb9f694bafec8f
Esther2504 <esther252000@gmail.com>
2023-09-19 20:17:12 +0200

    working on the api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [value, setValue] = useState('')
+  const [loggedIn, setLoggedIn] = useState(false)
+
+useEffect(() => {
+  var authOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/x-www-form-urlencoded'
+    },
+    body: 'grant_type=client_credentials&client_id=' + process.env.REACT_APP_CLIENT_ID + '&client_secret=' + process.env.REACT_APP_CLIENT_SECRET
+  };
+
+fetch('https://accounts.spotify.com/api/token', authOptions)
+.then(res => res.json())
+.then(data => console.log(data))
+}, [])
+
   return (
     <div className="App">
       <header className="App-header">
-<h1>Spotify playlist generator</h1>
-       <button>Use your own playlist</button>
-       <button>Use a public playlist</button>
+        <h1>Spotify playlist generator</h1>
+        <button>Use your own playlist</button>
+        <button>Use a public playlist</button>
       </header>
     </div>
   );
