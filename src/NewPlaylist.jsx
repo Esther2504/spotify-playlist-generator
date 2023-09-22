83e179468151adfb6b220f47609826fc28b45008
Esther2504 <esther252000@gmail.com>
2023-09-22 19:40:18 +0200

    next page

---
+++ src/NewPlaylist.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewPlaylist() {
+  return (
+    <div>NewPlaylist</div>
+  )
+}
--- src/PlaylistOptions.jsx
+++ src/PlaylistOptions.jsx
@@ -1,35 +1,58 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
+import NewPlaylist from './NewPlaylist'
 
-export default function Playlist( {playlistid, accessToken} ) {
-const [tracks, setTracks] = useState()
-const [songType, setSongType] = useState()
+export default function Playlist({ playlistid, accessToken }) {
+  const [tracks, setTracks] = useState()
+  const [songType, setSongType] = useState()
 
   const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20playlist-read-private`
 
-if (playlistid && !tracks) {
-axios
-.get(`https://api.spotify.com/v1/playlists/${playlistid}/tracks`, {
-  headers: {
-    Authorization: "Bearer " + accessToken,
-  },
-})
-.then((res) => {
-  console.log(res.data)
-  setTracks(res.data)
-})
-.catch((err) => {
-  console.log(err.response)
-})
+  if (playlistid && !tracks) {
+    axios
+      .get(`https://api.spotify.com/v1/playlists/${playlistid}/tracks`, {
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      })
+      .then((res) => {
+        console.log(res.data)
+        setTracks(res.data)
+      })
+      .catch((err) => {
+        console.log(err.response)
+      })
 
-}
+  }
 
-console.log(songType)
+  console.log(songType)
 
 
   return (
-    <Container>
+    <>
+      {!songType ?
+        <Container>
+          <h1>Create a new playlist with the...</h1>
+          <div />
+          <div />
+          <div>
+            <P onClick={() => setSongType("happiest")}>...happiest songs</P>
+            <P>...saddest songs</P>
+            <P>...energetic songs</P>
+            <P>...instrumental songs</P>
+            <P>...live songs</P>
+            <P>...songs in major key</P>
+            <P>...songs in minor key</P>
+            <P>...most danceable songs</P>
+            <P>...fastest songs</P>
+            <P>...slowest songs</P>
+          </div>
+        </Container>
+        :
+        <NewPlaylist tracks={tracks} />
+      }
+
       {/* {tracks ? <>
                       {tracks.items.map((track) =>
                         <P>{track.track.name}</P>
@@ -37,22 +60,7 @@ console.log(songType)
     </>
     :
     null} */}
-<h1>Create a new playlist with the...</h1>
-<div/>
-<div/>
-<div>
-<Radio type="radio">...happiest songs</Radio>
-<P>...saddest songs</P>
-<P>...energetic songs</P>
-<P>...instrumental songs</P>
-<P>...live songs</P>
-<P>...songs in major key</P>
-<P>...songs in minor key</P>
-<P>...most danceable songs</P>
-<P>...fastest songs</P>
-<P>...slowest songs</P>
-</div>
-    </Container>
+    </>
   )
 }
 
