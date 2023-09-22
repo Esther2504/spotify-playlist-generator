4b44698e3f52af104444abfc31ba46be0c4c0a55
Esther2504 <esther252000@gmail.com>
2023-09-22 19:28:50 +0200

    adding the playlist options, styling

--- src/PlaylistOptions.jsx
+++ src/PlaylistOptions.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import axios from 'axios'
 
 export default function Playlist( {playlistid, accessToken} ) {
 const [tracks, setTracks] = useState()
+const [songType, setSongType] = useState()
 
   const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20playlist-read-private`
 
@@ -23,17 +25,51 @@ axios
 
 }
 
+console.log(songType)
+
 
   return (
-    <div>
-      {tracks ? <>
+    <Container>
+      {/* {tracks ? <>
                       {tracks.items.map((track) =>
-                        <p>{track.track.name}</p>
+                        <P>{track.track.name}</P>
                     )}  
     </>
     :
-    null}
-
-    </div>
+    null} */}
+<h1>Create a new playlist with the...</h1>
+<div/>
+<div/>
+<div>
+<Radio type="radio">...happiest songs</Radio>
+<P>...saddest songs</P>
+<P>...energetic songs</P>
+<P>...instrumental songs</P>
+<P>...live songs</P>
+<P>...songs in major key</P>
+<P>...songs in minor key</P>
+<P>...most danceable songs</P>
+<P>...fastest songs</P>
+<P>...slowest songs</P>
+</div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+width: 90%;
+display: grid;
+grid-template-columns: 1fr 1fr;
+`
+
+const P = styled.p`
+cursor: pointer;
+
+&:hover {
+  color: #148255;
+}
+`
+
+const Radio = styled.input`
+list-type: none;
+`
