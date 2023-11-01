46e30064c32d297a0f1bbb4d5bbde2ed784d5b36
Esther2504 <esther252000@gmail.com>
2023-11-01 13:31:54 +0100

    bugfixes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [accessToken, setAccessToken] = useState()
   const [currentStep, setCurrentStep] = useState(1);
 
-  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20playlist-read-private%20playlist-modify-public%20playlist-modify-private`
+  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=https://esther2504.github.io/spotify-playlist-generator/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20playlist-read-private%20playlist-modify-public%20playlist-modify-private`
 
   const getAccessToken = window.location.hash.substring(14).split('&')[0]
 
