851bdc0c04148a7080e776236cfc11358a5e457e
Esther2504 <esther252000@gmail.com>
2023-11-04 12:04:56 +0100

    moving spinner to right page

--- src/NewPlaylist.jsx
+++ src/NewPlaylist.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import PlaylistCreated from './PlaylistCreated'
 import { createPlaylist, getAudioFeatures, getUser, addPlaylistTracks } from './APICalls'
 import NoSuitableSongs from './NoSuitableSongs'
+import styled from 'styled-components'
 
 export default function NewPlaylist({ tracks, songType, playlistName, data, accessToken }) {
   const [audioDetails, setaudioDetails] = useState()
@@ -213,7 +214,13 @@ export default function NewPlaylist({ tracks, songType, playlistName, data, acce
   return (
     <>
       {!playlistCreated && !noSongs ?
-        <p>Creating your playlist...</p>
+        <Spinner>
+          <Round />
+          <Round />
+          <Round />
+          <Round />
+          <Round />
+        </Spinner>
         : noSongs ?
           <NoSuitableSongs data={data} accessToken={accessToken} />
           :
@@ -223,3 +230,44 @@ export default function NewPlaylist({ tracks, songType, playlistName, data, acce
   )
 }
 
+const Spinner = styled.div`
+position: relative;
+`
+
+const Round = styled.div`
+  width: 15px;
+  height: 15px;
+  border-radius: 90px;
+  background: white;
+  animation: spinner 1.7s cubic-bezier(.38,.55,.8,.38) infinite;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+&:nth-child(2) {
+  animation: spinner 1.8s cubic-bezier(.38,.55,.8,.38) infinite;
+}
+
+&:nth-child(3) {
+  animation: spinner 1.9s cubic-bezier(.38,.55,.8,.38) infinite;
+}
+
+&:nth-child(4) {
+  animation: spinner 2s cubic-bezier(.38,.55,.8,.38) infinite;
+}
+
+&:nth-child(5) {
+  animation: spinner 2.1s cubic-bezier(.38,.55,.8,.38) infinite;
+}
+
+@keyframes spinner {
+  0% {
+    transform: rotate(0deg)
+    translate(-50px);
+  }
+  100% {
+    transform: rotate(360deg)
+    translate(-50px);
+  }
+}
+`
