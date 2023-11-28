3aadfc1b6ee1afcd16a909ca6f4ebd9817847b78
Esther2504 <esther252000@gmail.com>
2023-11-28 19:15:01 +0100

    improvements for the artist step

--- src/song-suggestions/Artists.jsx
+++ src/song-suggestions/Artists.jsx
@@ -31,9 +31,9 @@ if (artist.length > 2) {
 
   return (
     <>
+        <h2>Who are your favorite artists?</h2>
     <Container>
     <Label>     
-    <h2>Who are your favorite artists?</h2>
     <TextInput border={border} type="text" onChange={(e) => getArtistSuggestions(e.target.value)} />
     {artistSuggestions && artist.length > 2 ?
       <Suggestions>
@@ -43,12 +43,11 @@ if (artist.length > 2) {
     }
       </Label>
     <ArtistContainer>
-      {/* <h2>Chosen artists</h2> */}
-      {chosenArtists ?
+      {chosenArtists.length > 0 ?
         <Artists>
           {chosenArtists.map((suggestion) => <Artist><ArtistName><ArtistImg src={suggestion.images[0].url} />{suggestion.name}</ArtistName><SVG onClick={(e) => setChosenArtists(chosenArtists.filter(item => item !== suggestion))} viewBox="0 0 25 25" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" xmlnsSketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>cross</title> <defs> </defs> <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" sketchType="MSPage"> <g id="Icon-Set-Filled" sketchType="MSLayerGroup" transform="translate(-469.000000, -1041.000000)" fill="#ffffff"> <path d="M487.148,1053.48 L492.813,1047.82 C494.376,1046.26 494.376,1043.72 492.813,1042.16 C491.248,1040.59 488.712,1040.59 487.148,1042.16 L481.484,1047.82 L475.82,1042.16 C474.257,1040.59 471.721,1040.59 470.156,1042.16 C468.593,1043.72 468.593,1046.26 470.156,1047.82 L475.82,1053.48 L470.156,1059.15 C468.593,1060.71 468.593,1063.25 470.156,1064.81 C471.721,1066.38 474.257,1066.38 475.82,1064.81 L481.484,1059.15 L487.148,1064.81 C488.712,1066.38 491.248,1066.38 492.813,1064.81 C494.376,1063.25 494.376,1060.71 492.813,1059.15 L487.148,1053.48" id="cross" sketchType="MSShapeGroup"> </path> </g> </g> </g></SVG></Artist>)}
         </Artists>
-        : null
+        : <p>Your chosen artists will appear here. You can also continue without choosing an artist, just click on next step.</p>
       }
     </ArtistContainer>
     </Container>
@@ -58,10 +57,12 @@ if (artist.length > 2) {
 }
 
 const Container = styled.div`
-display: flex;
+display: grid;
 gap: 50px;
-width: 1500px;
-align-items: flex-end;
+width: 100%;
+max-width: 1500px;
+align-items: flex-start;
+grid-template-columns: 1fr 2fr;
 `
 
 const ArtistContainer = styled.div`
@@ -88,7 +89,7 @@ width: 500px;
 height: 40px;
 font-size: 1rem;
 border: 2px solid #148255;
-border-width: ${props => props.border ? "0" : "2px"};
+border-color: ${props => props.border ? "#148255 #148255 white" : "2px"};
 border-radius: ${props => props.border ? "10px 10px 0 0" : "10px"};
 padding: 0 10px;
 outline: none;
