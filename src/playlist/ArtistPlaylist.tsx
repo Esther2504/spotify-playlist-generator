d8041aac6eb974d7766fbc34a1f851a48b421de2
Esther2504 <esther252000@gmail.com>
2026-04-15 14:39:34 +0200

    create artist playlist + add tracks

--- src/playlist/ArtistPlaylist.tsx
+++ src/playlist/ArtistPlaylist.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import styled from 'styled-components'
+import axios from 'axios'
 
 type Props = {
     playlistid: string;
@@ -12,6 +13,7 @@ export default function ArtistPlaylist({ playlistid, playlistItems }: Props) {
     const [uniquePlaylistArtists, setUniquePlaylistArtists] = useState([])
     const [artist, setArtist] = useState<string>()
     const [uris, setUris] = useState([])
+    const [newPlaylistID, setNewPlaylistID] = useState()
 
     const params = useParams()
 
@@ -48,13 +50,68 @@ export default function ArtistPlaylist({ playlistid, playlistItems }: Props) {
         }
     }, [artist])
 
+    const accessToken = localStorage.getItem('accessToken')
+
+
+    useEffect(() => {
+
+        // https://api.spotify.com/v1/me/playlists
+
+        createPlaylist()
+
+
+
+    }, [uris])
+
+    function createPlaylist() {
+        axios
+            .post(`https://api.spotify.com/v1/me/playlists`, {
+                "name": "New Playlist",
+                "description": "New playlist description",
+                "public": false
+            }, {
+                headers: {
+                    Authorization: "Bearer " + accessToken,
+                },
+
+            })
+            .then((res) => {
+                console.log(res.data.id)
+                setNewPlaylistID(res.data.id)
+                addPlaylistItems(res.data.id)
+            })
+            .catch((err) => {
+                console.log(err)
+
+            })
+    }
+
+    function addPlaylistItems(playlist_id : string) {
+        axios
+            .post(`https://api.spotify.com/v1/playlists/${playlist_id}/items`, {
+               "uris": uris,
+    "position": 0
+            }, {
+                headers: {
+                    Authorization: "Bearer " + accessToken,
+                },
+
+            })
+            .then((res) => {
+                console.log(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+
+            })
+    }
+
     return (
         <Container>
             <p>For what artist do you want to create a seperate playlist?</p>
             {uniquePlaylistArtists.map((artist) => <Artist onClick={() => setArtist(artist)}>{artist}</Artist>)}
 
-
-
+<iframe data-testid="embed-iframe" src={`https://open.spotify.com/embed/playlist/${newPlaylistID}?utm_source=generator`} width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </Container>
     )
 }
