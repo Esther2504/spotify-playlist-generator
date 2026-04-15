33da7a72e0dd4ebd21f2980391b497cdcf0ffcd9
Esther2504 <esther252000@gmail.com>
2026-04-15 14:12:53 +0200

    get uris for new playlist

--- src/playlist/ArtistPlaylist.tsx
+++ src/playlist/ArtistPlaylist.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
+import styled from 'styled-components'
 
 type Props = {
     playlistid: string;
@@ -9,13 +10,15 @@ type Props = {
 export default function ArtistPlaylist({ playlistid, playlistItems }: Props) {
     const [playlistArtists, setPlaylistArtists] = useState([])
     const [uniquePlaylistArtists, setUniquePlaylistArtists] = useState([])
+    const [artist, setArtist] = useState<string>()
+    const [uris, setUris] = useState([])
 
     const params = useParams()
 
     const playlistID = params.playlistid
 
     console.log(playlistItems)
-    console.log(playlistArtists)
+    console.log(uris)
 
     useEffect(() => {
         playlistItems.forEach(element => {
@@ -23,17 +26,53 @@ export default function ArtistPlaylist({ playlistid, playlistItems }: Props) {
                 setPlaylistArtists((prevArtists) => [...prevArtists, artist.name])
             })
         });
-
     }, [])
 
     useEffect(() => {
         setUniquePlaylistArtists([...new Set(playlistArtists)])
     }, [playlistArtists])
 
+    useEffect(() => {
+        if (artist) {
+            playlistItems.forEach(element => {
+                console.log(element.item.artists)
+
+                element.item.artists.forEach(artistitem => {
+                    if (artistitem.name == artist) {
+                        console.log(artistitem.name)
+                        setUris((prevUris) => [...prevUris, element.item.uri])
+                    }
+                })
+
+            })
+        }
+    }, [artist])
+
     return (
-        <div>
+        <Container>
             <p>For what artist do you want to create a seperate playlist?</p>
-            {uniquePlaylistArtists.map((artist) => <p>{artist}</p>)}
-        </div>
+            {uniquePlaylistArtists.map((artist) => <Artist onClick={() => setArtist(artist)}>{artist}</Artist>)}
+
+
+
+        </Container>
     )
 }
+
+const Container = styled.div`
+display: flex;
+gap: 10px;
+flex-wrap: wrap;
+justify-content: center;
+
+p {
+width: 100%;
+}
+`
+
+const Artist = styled.div`
+background: var(--green);
+padding: 10px 20px;
+border-radius: 15px;
+cursor: pointer;
+`
