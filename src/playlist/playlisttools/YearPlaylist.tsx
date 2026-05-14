1e649488f81bd148132d0ffaaa2a7d4ec2a8fc06
Esther2504 <esther252000@gmail.com>
2026-05-14 15:27:34 +0200

    create playlist + add items

--- src/playlist/playlisttools/YearPlaylist.tsx
+++ src/playlist/playlisttools/YearPlaylist.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
 
 type Props = {
     playlistid: string;
@@ -12,8 +13,13 @@ export default function YearPlaylist({ playlistID, playlistItems, playlistName }
     const [playlistYears, setPlaylistYears] = useState([])
     const [uniqueYears, setUniqueYears] = useState([])
     const [selectedYear, setSelectedYear] = useState()
+    const [playlistReady, setPlaylistReady] = useState<boolean>(false)
+    const [uris, setUris] = useState([])
+    const [newPlaylistID, setNewPlaylistID] = useState<string>()
 
     console.log(playlistYears)
+
+    const accessToken = localStorage.getItem('accessToken')
 
     useEffect(() => {
         console.log(playlistItems)
@@ -28,12 +34,116 @@ export default function YearPlaylist({ playlistID, playlistItems, playlistName }
         setUniqueYears([...new Set(playlistYears)])
     }, [playlistYears])
 
+    useEffect(() => {
+        if (selectedYear) {
+            console.log(playlistItems)
+            playlistItems.forEach(element => {
+
+                let releaseDate = new Date(element?.item?.album?.release_date)
+            let releaseYear = releaseDate.getFullYear()
+
+            console.log(releaseYear)
+            console.log(selectedYear)
+
+                if (releaseYear == selectedYear) {
+                    setUris((prevUris) => [...prevUris, element?.item?.uri])
+                }
+
+            })
+        }
+    }, [selectedYear])
+
+    useEffect(() => {
+        if (!newPlaylistID && uris.length > 0) {
+            createPlaylist(selectedYear)
+        }
+    }, [selectedYear, uris])
+
+    function createPlaylist(selectedYear: string) {
+console.log(uris)
+        axios
+            .post(`https://api.spotify.com/v1/me/playlists`, {
+                "name": `Seperate year playlist created from ${playlistName}`,
+                "description": "",
+                "public": false
+            }, {
+                headers: {
+                    Authorization: "Bearer " + accessToken,
+                },
+
+            })
+            .then((res) => {
+                console.log(res.data.id)
+                setNewPlaylistID(res.data.id)
+                addPlaylistItems(res.data.id, uris)
+            })
+            .catch((err) => {
+                console.log(err)
+
+            })
+    }
+
+    function addPlaylistItems(playlist_id: string, uri_items: any) {
+        if (uri_items.length == 0) {
+            return;
+        }
+        let max_uris = uri_items;
+        let leftovers = null;
+        if (uri_items.length > 100) {
+            max_uris = uri_items.slice(0, 99)
+            leftovers = uri_items.slice(100, uri_items.length)
+        }
+
+        console.log(max_uris)
+        console.log(leftovers)
+
+        axios
+            .post(`https://api.spotify.com/v1/playlists/${playlist_id}/items`, {
+                "uris": max_uris,
+                "position": 0
+            }, {
+                headers: {
+                    Authorization: "Bearer " + accessToken,
+                },
+
+            })
+            .then((res) => {
+                console.log(res.data)
+
+                if (leftovers) {
+                    addPlaylistItems(playlist_id, leftovers)
+                } else {
+                    setPlaylistReady(true)
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+
+            })
+    }
+
+
     return (
         <Container>
-            <h1>Which year do you want to create a playlist of?</h1>
-            <YearContainer>
-                {uniqueYears.sort().map((year) => <Year onClick={() => setSelectedYear(year)}>{year}</Year>)}
-            </YearContainer>
+            {playlistReady ?
+                <ReadyContainer>
+                    <div>
+                        <h1>Your playlist is ready!</h1>
+                        <p>We already saved the playlist to your Spotify.</p>
+                        <Button onClick={() => { setPlaylistReady(false); setUris([]) }}>Add another artist to this playlist</Button>
+                        {/* <Button onClick={() => { setPlaylistReady(false); setUris([]), setNewPlaylistID(undefined) }}>Create new playlist with another artist</Button> */}
+                    </div>
+
+                    <iframe data-testid="embed-iframe" src={`https://open.spotify.com/embed/playlist/${newPlaylistID}?utm_source=generator`} width="100%" height="352" frameBorder="0" allowFullScreen={true} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                </ReadyContainer>
+                :
+                <>
+                    <h1>Which year do you want to create a playlist of?</h1>
+                    <YearContainer>
+                        {uniqueYears.sort().map((year) => <Year onClick={() => setSelectedYear(year)}>{year}</Year>)}
+                    </YearContainer>
+                </>
+            }
         </Container>
     )
 }
@@ -50,6 +160,24 @@ scroll-behavior: smooth;
 
 h1 {
 font-size: 2.6rem;
+}
+`
+
+const ReadyContainer = styled.div` 
+display: flex;
+gap: 50px;
+justify-content: space-evenly;
+width: 100%;
+
+div {
+display: flex;
+flex-direction: column;
+gap: 20px;
+}
+
+iframe {
+max-width: 600px;
+min-height: 600px;
 }
 `
 
@@ -70,4 +198,17 @@ border: 2px solid transparent;
 &:hover {
 border: 2px solid #fff;
 }
+`
+
+const Button = styled.button`
+background: #148255;
+color: #fff;
+padding: 10px 20px;
+text-decoration: none;
+border-radius: 20px;
+margin-right: auto;
+font-weight: bold;
+border: none;
+font-size: 1rem;
+cursor: pointer;
 `
